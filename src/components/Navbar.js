2dae7014029a38ecfcc62db5356c664926f50501
Rob Martin <rob.martin.5758@gmail.com>
2020-06-25 12:22:09 -0400

    Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Church of the Epiphany" style={{ width: '20%' }} />
             </Link>
             {/* Hamburger menu */}
             <div
